21a63155544c3ced90d898668c3f789e16995fcf
Glenn Cullen <glennncullen@gmail.com>
2016-11-16 18:56:54 +0000

    initial commit

---
+++ Quizzer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quizzer.Startup))]
+namespace Quizzer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
